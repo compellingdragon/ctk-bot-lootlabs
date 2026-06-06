910e6ab46db9a826f2e3eca40c38a8d12cb0f01f
Vishesh Saluja <compellingdragon@gmail.com>
2026-06-06 21:24:48 +0530

    Update linkvertise.js

--- src/linkvertise.js
+++ src/linkvertise.js
@@ -1,1 +1,16 @@
+const config = require('./config');
 
+function makeLinkvertiseUrl(destinationUrl) {
+  if (!config.linkvertiseUserId) {
+    throw new Error('Missing LINKVERTISE_USER_ID');
+  }
+
+  const encoded = Buffer.from(destinationUrl).toString('base64');
+  const randomPath = (Math.random() * 1000).toFixed(12);
+
+  return `https://link-to.net/${config.linkvertiseUserId}/${randomPath}/dynamic?r=${encodeURIComponent(encoded)}`;
+}
+
+module.exports = {
+  makeLinkvertiseUrl
+};
