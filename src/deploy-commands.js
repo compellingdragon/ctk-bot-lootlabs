352f954a1631622940ee4c75830b55d0d67b7455
Vishesh Saluja <compellingdragon@gmail.com>
2026-06-06 21:29:23 +0530

    Update deploy-commands.js

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -4,12 +4,24 @@ const config = require('./config');
 const commands = [
   new SlashCommandBuilder()
     .setName('earn')
-    .setDescription('Create a private CTK earn session link.'),
+    .setDescription('Create a private CTK earn session link.')
+    .addStringOption(opt =>
+      opt
+        .setName('method')
+        .setDescription('Choose earning method')
+        .setRequired(false)
+        .addChoices(
+          { name: 'LootLabs - higher CTK', value: 'lootlabs' },
+          { name: 'Linkvertise - quick CTK', value: 'linkvertise' }
+        )
+    ),
 
   new SlashCommandBuilder()
     .setName('balance')
     .setDescription('Check your CTK balance or another user balance.')
-    .addUserOption(opt => opt.setName('user').setDescription('Optional user to check').setRequired(false)),
+    .addUserOption(opt =>
+      opt.setName('user').setDescription('Optional user to check').setRequired(false)
+    ),
 
   new SlashCommandBuilder()
     .setName('leaderboard')
@@ -18,8 +30,12 @@ const commands = [
   new SlashCommandBuilder()
     .setName('redeem')
     .setDescription('Request a manual server redemption using your CTK.')
-    .addIntegerOption(opt => opt.setName('amount').setDescription('CTK amount to redeem').setMinValue(1).setRequired(true))
-    .addStringOption(opt => opt.setName('item').setDescription('What you want to redeem').setRequired(true)),
+    .addIntegerOption(opt =>
+      opt.setName('amount').setDescription('CTK amount to redeem').setMinValue(1).setRequired(true)
+    )
+    .addStringOption(opt =>
+      opt.setName('item').setDescription('What you want to redeem').setRequired(true)
+    ),
 
   new SlashCommandBuilder()
     .setName('admin-add')
@@ -67,5 +83,7 @@ async function main() {
 
 main().catch(err => {
   console.error(err);
+  process.exit(1);
+});
   process.exit(1);
 });
